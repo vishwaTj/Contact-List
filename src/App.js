77afb941acd5a67e7f08600a3711acab58609dae
Tj_ <tjvishwa88@gmail.com>
2023-03-04 21:10:00 +0530

    basic set up for input set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 
 function App() {
+  const FetchData = async() =>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  
+  }
+  FetchData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <div>
+         <div id="Main-container">
+            <header>
+              <h1>Contact List</h1>
+            </header>
+              <div className='input-container'>
+                <form>
+                  <label className='label'>Name</label>
+                  <input className='input' type="text" placeholder='Name'></input>
+                  <label className='label'>Number</label>
+                  <input className='input' type="text" placeholder='number'></input>
+                  <label className='label'>Email</label>
+                  <input className='input' type="email" placeholder='Email'></input>
+                  <button type="submit">Submit</button>
+                </form>
+              </div> 
+         </div>
+       </div>    
+    </>
   );
 }
 
